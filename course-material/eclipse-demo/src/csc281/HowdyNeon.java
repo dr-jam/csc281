70879749eb8550e883b97b6cb4925229d82abcc1
Josh McCoy <joshua.allen.mccoy@gmail.com>
2017-04-24 09:06:26 -0400

    HowdyNeon

---
+++ course-material/eclipse-demo/src/csc281/HowdyNeon.java
@@ -0,0 +1,16 @@
+package csc281;
+
+public class HowdyNeon {
+
+	public static String helloBark = "Howdy!";
+
+	public static void main(String[] args) {
+		int counter = 0;
+		// TODO Auto-generated method stub
+		while (counter < 2) {
+			System.out.println(helloBark);
+			++counter;
+		}
+	}
+
+}
